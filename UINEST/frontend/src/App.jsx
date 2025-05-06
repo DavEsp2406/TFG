148da1bad156dc16e8e9604e60bd58edb9094191
David <davidespeton@gmail.com>
2025-05-07 00:10:02 +0200

    Creacion de paginas de login y configuracion de axios y zustand

--- UINEST/frontend/src/App.jsx
+++ UINEST/frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LogInPage from './pages/LogInPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
 const App = () => {
   return (
-    <div className='text-red-600'>HELLO WORLD</div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </>
   )
 }
 
