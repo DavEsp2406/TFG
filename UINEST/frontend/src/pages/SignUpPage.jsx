d204319b0a369f35fdf0d964c7b06a8743d2b777
David <davidespeton@gmail.com>
2025-05-14 12:51:50 +0200

    Implementado sign up

--- UINEST/frontend/src/pages/SignUpPage.jsx
+++ UINEST/frontend/src/pages/SignUpPage.jsx
@@ -1,37 +1,48 @@
-import {useState} from "react"
+import { useState } from "react"
 import { useAuthStore } from "../store/useAuthStore"
 import { MessageSquare, User, Mail, Lock, Eye, EyeOff, Loader, Loader2 } from "lucide-react"
 import { Link } from "react-router-dom"
+import toast from "react-hot-toast"
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
-    fullName:"",
-    email:"",
-    password:""
+    fullName: "",
+    email: "",
+    password: ""
   })
 
-  const {signup, isSigningUp} = useAuthStore()
+  const { signup, isSigningUp } = useAuthStore()
 
-  const validateForm = () => {}
+  const validateForm = () => { 
+    if (!formData.fullName.trim()) return toast.error("Full name is required")
+    if (!formData.email.trim()) return toast.error("Email is required")
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) return toast.error("Invalid email format")
+    if (!formData.password) return toast.error("Password is required")
+    if (formData.password.length < 6) return toast.error("Password must be at least 6 characters")
+
+    return true
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const success = validateForm()
+
+    if (success === true) signup(formData)
   }
 
   return (
-    <div className="grid lg:grid-cols-2 min-h-screen">
-      {/*left side*/}
+    <div className="flex flex-col justify-center min-h-screen">
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="space-y-8 w-full max-w-md">
           {/* LOGO */}
-          <div className="mb-8 text-center">
-            <div className="group flex flex-col items-center gap-2">
-              <div className="flex justify-center items-center bg-primary/10 group-hover:bg-primary/20 rounded-xl size-12 transition-colors">
-                <MessageSquare className="size-6 text-primary"/>
-              </div>
-              <h1 className="mt-2 font-bold text-2xl">Create account</h1>
-              <p className="text-base-content/60">Get started with your free account</p>
+          <div className="group flex flex-col items-center gap-2 mb-8 text-center">
+            <div className="flex justify-center items-center bg-primary/10 group-hover:bg-primary/20 rounded-xl size-12 transition-colors">
+              <MessageSquare className="size-6 text-primary" />
             </div>
+            <h1 className="mt-2 font-bold text-2xl">Create account</h1>
+            <p className="text-base-content/60">Get started with your free account</p>
           </div>
 
           <form onSubmit={handleSubmit} className="space-y-6">
@@ -104,7 +115,7 @@ const SignUpPage = () => {
                   <Loader2 className="size-5 animate-spin" />
                   Loading ...
                 </>
-              ): (
+              ) : (
                 "Create Account"
               )}
             </button>
@@ -112,15 +123,15 @@ const SignUpPage = () => {
           <div className="text-center">
             <p className="text-base-content/60">
               Already have an account?{" "}
-              <Link to= "/login" className="link link-primary">
-              Sign in
+              <Link to="/login" className="link link-primary">
+                Sign in
               </Link>
             </p>
           </div>
         </div>
       </div>
     </div>
-  ) 
+  )
 }
 
 export default SignUpPage
