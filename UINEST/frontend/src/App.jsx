fdf575443f6b2cbe97b6e21bd993be8ac46b7303
David <davidespeton@gmail.com>
2025-05-19 18:53:37 +0200

    Agregada pagina de settings con temas

--- UINEST/frontend/src/App.jsx
+++ UINEST/frontend/src/App.jsx
@@ -4,41 +4,42 @@ import SignUpPage from './pages/SignUpPage.jsx'
 import LogInPage from './pages/LogInPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
- 
 
-import {Routes, Route, Navigate} from 'react-router-dom'
+
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthStore } from './store/useAuthStore.js'
 import { useEffect } from 'react'
 
-import {Loader} from "lucide-react"
-import {Toaster} from "react-hot-toast"
+import { Loader } from "lucide-react"
+import { Toaster } from "react-hot-toast"
+import { useThemeStore } from './store/useThemeStore.js'
 
 const App = () => {
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
-
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const theme = useThemeStore((state) => state.theme)
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
 
-  console.log({authUser})
+  console.log({ authUser })
 
-  if(isCheckingAuth && !authUser) return (
+  if (isCheckingAuth && !authUser) return (
     <div className='flex justify-center items-center h-screen'>
-      <Loader className='size-10 animate-spin'/>
+      <Loader className='size-10 animate-spin' />
     </div>
   )
 
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to= "/login"/>} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to= "/"/>} />
-        <Route path="/login" element={!authUser ? <LogInPage /> : <Navigate to= "/" />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LogInPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={ authUser ? <ProfilePage /> : <Navigate to= "/login"/>} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster />
--- UINEST/frontend/src/pages/SettingsPage.jsx
+++ UINEST/frontend/src/pages/SettingsPage.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useThemeStore } from "../store/useThemeStore"
+import { THEMES } from "../constants"
+import { Send } from "lucide-react"
+
+const PREVIEW_MESSAGES = [
+  { id: 1, content: "It's over, Anakin. I have the high ground.", isSent: false },
+  { id: 2, content: "You underestimate my power!", isSent: true }
+]
 
 const SettingsPage = () => {
+  const { theme, setTheme } = useThemeStore()
+
   return (
-    <div>SettingsPage</div>
+    <div className="mx-auto px-4 pt-20 max-w-5xl h-screen container">
+      <div className="space-y-6">
+        <div className="flex flex-col gap-1">
+          <h2 className="font-semibold text-lg">Theme</h2>
+          <p className="text-sm text-base-content/70">Choose a theme for your chat</p>
+        </div>
+        <div className="gap-2 grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8">
+          {THEMES.map((t) => (
+            <button
+              key={t}
+              className={`
+                group flex flex-col items-center gap-1.5 p-2 rounded-lg transition-colors
+                ${theme === t ? "bg-base-200" : "hover:bg-base-200/50"}
+                `}
+              onClick={() => setTheme(t)}
+            >
+              <div className="relative rounded-md w-full h-8 overflow-hidden" data-theme={t}>
+                <div className="absolute inset-0 gap-px grid grid-cols-4 p-1">
+                  <div className="bg-primary rounded"></div>
+                  <div className="bg-secondary rounded"></div>
+                  <div className="bg-accent rounded"></div>
+                  <div className="bg-neutral rounded"></div>
+                </div>
+              </div>
+              <span className="w-full font-medium text-[11px] text-center truncate">
+                {t.charAt(0).toUpperCase() + t.slice(1)}
+              </span>
+            </button>
+          ))}
+        </div>
+
+      </div>
+    </div>
   )
 }
 
