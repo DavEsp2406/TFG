1b538b61b2c36d16c2ab2251b2eac935a6bcc103
David Espeton <david.espeton@servinnova.local>
2025-06-02 16:44:11 +0200

    Añadido ruta componentes

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import path from "path"
 import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import componentRoutes from "./routes/component.route.js"
 import { app, server } from "./lib/socket.js"
 
 dotenv.config()
@@ -25,6 +26,7 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/components", componentRoutes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
